4c61b8142dfa5b12fe3761fff97f66af74f47b96
Romain Menke <romainmenke@gmail.com>
2015-11-12 17:35:06 +0100

    Initial Commit

---
+++ NodeTree/NodeTree.h
@@ -0,0 +1,19 @@
+//
+//  NodeTree.h
+//  NodeTree
+//
+//  Created by Romain Menke on 12/11/15.
+//  Copyright © 2015 Romain Menke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NodeTree.
+FOUNDATION_EXPORT double NodeTreeVersionNumber;
+
+//! Project version string for NodeTree.
+FOUNDATION_EXPORT const unsigned char NodeTreeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NodeTree/PublicHeader.h>
+
+
